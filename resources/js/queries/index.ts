e91e8353d599dddd362d6e3f23b80fda3951eec2
Prathamesh <prathameshvaze@gmail.com>
2025-09-18 23:24:31 +0530

    Episode 2 WIP

--- resources/js/queries/index.ts
+++ resources/js/queries/index.ts
@@ -1,21 +1,16 @@
-import { Puppy } from "../types";
 
 export async function getPuppies() {
-  try {
-    const response = await fetch("https://react-from-scratch-api.test/api/puppies", {
-      headers: {
-        accept: "application/json",
-      },
-    });
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw errorData;
-    }
-    const { data } = await response.json();
-    return data;
-  } catch (error) {
-    throw error;
+  const response = await fetch("https://react-from-scratch-api.test/api/puppies", {
+    headers: {
+      accept: "application/json",
+    },
+  });
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw errorData;
   }
+  const { data } = await response.json();
+  return data;
 }
 
 export async function toggleLikedByStatus(id: number){
